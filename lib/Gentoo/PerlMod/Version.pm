c1c518bcfec34affb04f35b7a7a9d89ca37cf910
Kent Fredric <kentfredric@gmail.com>
2011-01-10 07:16:26 +1300

    Basic first implementation is ready, tests up, TODO: more docus

--- lib/Gentoo/PerlMod/Version.pm
+++ lib/Gentoo/PerlMod/Version.pm
@@ -1,5 +1,220 @@
 use strict;
 use warnings;
+
 package Gentoo::PerlMod::Version;
+
+# ABSTRACT: Convert arbitrary Perl Modules' versions into normalised Gentoo versions.
+
+use Sub::Exporter -setup => { exports => [qw( gentooize_version)] };
+use Carp;
+use List::MoreUtils qw( natatime );
+
+=head1 SYNOPSIS
+
+
+    use Gentoo::PerlMod::Version qw( :all );
+
+    # http://search.cpan.org/~gmpassos/XML-Smart-1.6.9/
+    say gentooize_version( '1.6.9' )  # 1.006.009
+
+    http://search.cpan.org/~pip/Math-BaseCnv-1.6.A6FGHKE/
+
+    say gentooize('1.6.A6FGHKE')   #  <-- death, this is awful
+
+    # -- Work-In-Progress Features --
+
+    say gentooize('1.6.A6FGHKE',{ lax => 1}) # <-- still death
+
+    say gentooize('1.6.A6FGHKE',{ lax => 2}) # 1.006.366.556.632.014  # <-- the best we can do.
+
+    say gentooize('1.9902-TRIAL')   #  <-- death, this is awful
+
+    say gentooize('1.9902-TRIAL', { lax => 1 })   #  1.990.200 # <-- -TRIAL gets nuked.
+
+
+
+=cut
+
+=method gentooize_version
+
+    my $normalized = gentoo_version( $weird_version )
+
+gentooize_version tries hard to mangle a version thats part of a CPAN dist into a normalized form
+for Gentoo.
+
+Normal behaviour accepts only sane non-testing versions, and expands them to the form of \d(.\d\d\d)+ ie:
+
+    0.1         -> 0.100
+    0.001       -> 0.001
+    1.1         -> 1.100
+    1.123.13    -> 1.123.013
+
+Etc.
+
+This uses L<< C<version.pm>|version >> to read versions and to normalize them to floatingpoint form, and the floating point form
+is sliced into arbitrary parts 3-digits long. ie:
+
+    $x = version->parse( 0.01 )->numify;   # 0.010
+    $x =~ s/(\.\d\d\d)(\d+)$/$1.$2/;       # 0.010
+    $x = version->parse( 0.001 )->numify;  # 0.001
+    $x =~ s/(\.\d\d\d)(\d+)$/$1.$2/;       # 0.001
+    $x = version->parse( 0.0001 )->numify; # 0.000100
+    $x =~ s/(\.\d\d\d)(\d+)$/$1.$2/;       # 0.000.100
+
+So assuming Perl can handle your versions, they can be normalised.
+
+=head3 lax level 1
+
+    my $nomralized = gentoo_version( $werid_version, { lax => 1 } );
+
+B<EXPERIMENTAL:> This feature is still in flux, and the emitted versions may change.
+
+This adds one layer of laxitifity, and permits parsing and processing of "Developer Release" builds.
+
+
+
+
+
+=cut
+
+sub gentooize_version {
+  my ( $perlver, $config ) = @_;
+  $config ||= {};
+  $config->{lax} = 0 unless defined $config->{lax};
+
+  if ( not _has_bad_bits($perlver) ) {
+    return _lax_cleaning_0($perlver);
+  }
+
+  if ( $config->{lax} eq 1 ) {
+    return _lax_cleaning_1($perlver);
+  }
+  if ( $config->{lax} eq 2 ) {
+    return _lax_cleaning_2($perlver);
+  }
+
+  Carp::croak("Invalid version format (non-numeric data). ( set { lax => } for more permissive behaviour )");
+}
+
+my $char_map = {
+  ( map { $_ => $_ } 0 .. 9 ),    # 0..9
+  ( map { chr( $_ + 65 ) => $_ + 10 } 0 .. 25 ),    # A-Z
+  ( map { chr( $_ + 97 ) => $_ + 10 } 0 .. 25 )     # a-z
+};
+
+sub _char_map {
+  require Data::Dumper;
+  local $Data::Dumper::Sortkeys = 1;
+  local $Data::Dumper::Terse    = 1;
+  local $Data::Dumper::Indent   = 0;
+  return Data::Dumper::Dumper($char_map);
+}
+
+sub _code_for {
+  my $char = shift;
+  if ( !exists $char_map->{$char} ) {
+    Carp::croak( "Character $char ( " . ord($char) . ") is not in the ascii-to-int translation table" );
+  }
+  return $char_map->{$char};
+}
+
+sub _enc_pair {
+  my (@tokens) = @_;
+  if ( not @tokens ) {
+    return '';
+  }
+  if ( @tokens < 2 ) {
+    return _code_for( shift(@tokens) );
+  }
+  return ( _code_for( $tokens[0] ) * 36 ) + ( _code_for( $tokens[1] ) );
+}
+
+sub _ascii_to_int {
+  my $string = shift;
+  my @chars = split //, $string;
+  my @output;
+
+  my $iterator = natatime 2, @chars;
+  while ( my @vals = $iterator->() ) {
+    push @output, _enc_pair(@vals);
+  }
+
+  return join '.', @output;
+}
+
+sub _has_bad_bits {
+  shift(@_) =~ /[^0-9.]/;
+}
+
+sub _lax_cleaning_0 {
+    my $version = shift;
+    return _expand_numeric( $version );
+}
+
+
+sub _lax_cleaning_1 {
+  my $version = shift;
+  my $istrial = 0;
+  my $prereleasever = undef;
+
+  if(  $version =~ s/-TRIAL$// ){
+      $istrial = 1;
+  }
+  if( $version =~ s/_(.*)$// ){
+     $prereleasever = "$1";
+     if( $prereleasever =~ /_/ ){
+         Carp::croak("More than one _ in a version is not permitted");
+     }
+  }
+  $version = _expand_numeric( $version );
+  if( $istrial and not defined $prereleasever ){
+      $version .= '_pre001';
+  } elsif( defined $prereleasever ){
+      $prereleasever = _expand_numeric( '1.' . $prereleasever );
+      $prereleasever =~ s/^1.//;
+      $version .= '_pre' . $prereleasever;
+  }
+  return $version;
+}
+
+sub _lax_cleaning_2 {
+  my $version = shift;
+  my $istrial = 0;
+
+  if(  $version =~ s/-TRIAL$// ){
+    $istrial = 1;
+  }
+
+  my @parts = split /([._])/, $version;
+  my @out;
+  for( @parts ) {
+      if( $_ =~ /^[_.]$/ ){
+          push @out, $_;
+          next;
+      }
+      if( not _has_bad_bits($_ ) ){
+          push @out, $_;
+          next;
+      }
+      push @out, _ascii_to_int( $_ );
+  }
+
+  my $version_out = join q{}, @out;
+  if( $istrial ){
+      $version_out .= '-TRIAL';
+  }
+  return _lax_cleaning_1( $version_out );
+}
+
+sub _expand_numeric {
+  my $perlver = shift;
+
+  my $numeric = version->parse($perlver)->numify;
+
+  1 while $numeric =~ s/(\.\d\d\d)(\d+)$/$1.$2/;
+
+  return $numeric;
+}
+
 
 1;
---
+++ t/01_basic.t
@@ -0,0 +1,86 @@
+use strict;
+use warnings;
+
+use Test::More 0.96;
+use Test::Fatal;
+use Gentoo::PerlMod::Version qw( :all );
+
+sub b {
+  my ( $z, $x, $y ) = @_;
+  is( gentooize_version($x), $y, "$x -> $y expanding ( icode $z )" );
+}
+
+# 1..10
+b( 1,  '0',        '0.000' );
+b( 2,  '1',        '1.000' );
+b( 3,  '0.1',      '0.100' );
+b( 4,  '1.1',      '1.100' );
+b( 5,  '0.01',     '0.010' );
+b( 6,  '1.01',     '1.010' );
+b( 7,  '1.001',    '1.001' );
+b( 8,  '1.0001',   '1.000.100' );
+b( 9,  '1.00001',  '1.000.010' );
+b( 10, '1.000001', '1.000.001' );
+;    # 10 .. 19
+b( 11, '1.0000001',      '1.000.000.100' );
+b( 12, '1.00000001',     '1.000.000.010' );
+b( 13, '1.000000001',    '1.000.000.001' );
+b( 14, '1.0000000001',   '1.000.000.000.100' );
+b( 15, '1.00000000001',  '1.000.000.000.010' );
+b( 16, '1.000000000001', '1.000.000.000.001' );
+b( 17, '1.0.1',          '1.000.001' );
+b( 18, '1.0.01',         '1.000.001' );
+b( 19, '1.0.001',        '1.000.001' );
+b( 20, '1.0.10',         '1.000.010' );
+;    # 20 .. 29
+b( 21, '1.0.010',          '1.000.010' );
+b( 22, '1.0.0010',         '1.000.010' );
+b( 23, '1.1.1',            '1.001.001' );
+b( 24, '1.1.01',           '1.001.001' );
+b( 25, '1.1.001',          '1.001.001' );
+b( 26, '1.1.10',           '1.001.010' );
+b( 27, '1.1.010',          '1.001.010' );
+b( 28, '1.1.0010',         '1.001.010' );
+b( 29, '1.1.000000000010', '1.001.010' );
+b( 30, '1.10.1',           '1.010.001' );
+;    # 30 .. 39
+b( 31, '1.10.01',           '1.010.001' );
+b( 32, '1.10.001',          '1.010.001' );
+b( 33, '1.10.10',           '1.010.010' );
+b( 34, '1.10.010',          '1.010.010' );
+b( 35, '1.10.0010',         '1.010.010' );
+b( 36, '1.10.000000000010', '1.010.010' );
+b( 37, '1.010.1',           '1.010.001' );
+b( 38, '1.010.01',          '1.010.001' );
+b( 39, '1.010.001',         '1.010.001' );
+b( 40, '1.010.10',          '1.010.010' );
+;    # 40 .. 49
+b( 41, '1.010.010',          '1.010.010' );
+b( 42, '1.010.0010',         '1.010.010' );
+b( 43, '1.010.000000000010', '1.010.010' );
+
+my $e;
+isnt( $e = exception { gentooize_version('1.6.A6FGHKE') }, undef, 'Ascii is bad' );     # 44
+isnt( $e = exception { gentooize_version('1.6-TRIAL') },   undef, '-TRIAL is bad' );    # 45
+isnt( $e = exception { gentooize_version('1.6_0') },       undef, 'x_y is bad' );       # 46
+isnt( $e = exception { gentooize_version( '1.6.A6FGHKE', { lax => 1 } ) }, undef, 'Ascii is bad ( even with lax => 1 )' );    # 47
+is( $e = exception { gentooize_version( '1.6-TRIAL',   { lax => 1 } ) }, undef, '-TRIAL is ok with lax => 1' );               # 48
+is( $e = exception { gentooize_version( '1.6_0',       { lax => 1 } ) }, undef, 'x_y is ok with lax => 1 ' );                 # 49
+is( $e = exception { gentooize_version( '1.6.A6FGHKE', { lax => 2 } ) }, undef, 'Ascii is ok with lax => 2 )' );              # 50
+is( $e = exception { gentooize_version( '1.6-TRIAL',   { lax => 2 } ) }, undef, '-TRIAL is ok with lax => 2' );               # 51
+is( $e = exception { gentooize_version( '1.6_0',       { lax => 2 } ) }, undef, 'x_y is ok with lax => 2 ' );                 # 52
+
+is( gentooize_version( '1.6-TRIAL',   { lax => 1 } ), '1.600_pre001', 'x.y-TRIAL' );                                          # 53
+is( gentooize_version( '1.67-TRIAL',  { lax => 1 } ), '1.670_pre001', 'x.yy-TRIAL' );                                         # 54
+is( gentooize_version( '1.675-TRIAL', { lax => 1 } ), '1.675_pre001', 'x.yyy-TRIAL' );                                        # 55
+is( gentooize_version( '1.6_01',      { lax => 1 } ), '1.600_pre010', 'x.y_z' );                                              # 56
+is( gentooize_version( '1.67_01',     { lax => 1 } ), '1.670_pre010', 'x.yy_zz' );                                            # 57
+is( gentooize_version( '1.675_01',    { lax => 1 } ), '1.675_pre010', 'x.yyy_zz' );                                           # 58
+
+isnt( $e = exception { gentooize_version( '1.6_01_01', { lax => 1 } ) }, undef, 'x.y_z_a fails' );                            # 59
+is( gentooize_version( '1.6.A',    { lax => 2 } ), '1.006.010',     'x.y.ASCII' );
+is( gentooize_version( '1.6.AA',   { lax => 2 } ), '1.006.370',     'x.y.ASCII' );
+is( gentooize_version( '1.6.AAA',  { lax => 2 } ), '1.006.370.010', 'x.y.ASCII' );
+is( gentooize_version( '1.6.AAAA', { lax => 2 } ), '1.006.370.370', 'x.y.ASCII' );
+
+done_testing;
