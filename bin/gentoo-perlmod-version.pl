1c0a7b7d77376e64820a9c8b58984fa4acc81ee6
Kent Fredric <kentfredric@gmail.com>
2014-09-27 17:33:57 +1200

    Subroutines::ProhibitCallsToUndeclaredSubs

--- bin/gentoo-perlmod-version.pl
+++ bin/gentoo-perlmod-version.pl
@@ -14,7 +14,7 @@ our $VERSION = '0.7.0';
 # AUTHORITY
 
 ## no critic (ProhibitPunctuationVar)
-use Gentoo::PerlMod::Version qw( :all );
+use Gentoo::PerlMod::Version qw( gentooize_version );
 use Carp qw( croak );
 
 =head1 SYNOPSIS
